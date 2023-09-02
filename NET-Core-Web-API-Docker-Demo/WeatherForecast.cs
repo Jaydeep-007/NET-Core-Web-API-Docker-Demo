55396a9d44cdf95c88f416320340b16b985c2f8c
Jaydeep-07 <jaydeepvpatil225@gmail.com>
2023-09-02 11:21:38 +0530

    Add project files.

---
+++ NET-Core-Web-API-Docker-Demo/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace NET_Core_Web_API_Docker_Demo
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
